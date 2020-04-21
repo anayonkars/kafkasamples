4ccc31dbdd13c554bb428c1ffd0f4437fda5085b
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2020-04-21 14:55:03 +0530

    Refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
