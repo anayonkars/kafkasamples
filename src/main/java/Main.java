df39fcd5b3d817ffa73ee973b8e743cca10162d7
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2020-04-21 15:34:13 +0530

    Created Producer and Consumer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,55 @@
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
-import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class Main {
 
     public static final String KAFKA_BROKERS = "localhost:9092";
+    public static final String KAFKA_TOPIC = "mytopic";
 
     public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
-        properties.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        KafkaProducer producer = new KafkaProducer(properties);
+        Properties producerProperties = new Properties();
+        producerProperties.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+        producerProperties.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        KafkaProducer producer = new KafkaProducer(producerProperties);
+        Properties consumerProperties = new Properties();
+        consumerProperties.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+        consumerProperties.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerProperties.put(GROUP_ID_CONFIG, "myconsumergroup");
+        consumerProperties.put(MAX_POLL_RECORDS_CONFIG, 10);
+        consumerProperties.put(ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumerProperties.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
+        KafkaConsumer<Long, String> consumer = new KafkaConsumer(consumerProperties);
         try {
-            ProducerRecord myMessage = new ProducerRecord("mytopic", "myMessage");
+            consumer.subscribe(Collections.singletonList(KAFKA_TOPIC));
+            ProducerRecord myMessage = new ProducerRecord(KAFKA_TOPIC, "myMessage");
             producer.send(myMessage);
             System.out.println("Sent " + myMessage);
-            ProducerRecord myMessage2 = new ProducerRecord("mytopic", 0, System.currentTimeMillis(), "mykey", "myValue");
-            producer.send(myMessage2);
+            /*ProducerRecord myMessage2 = new ProducerRecord(KAFKA_TOPIC, 0, System.currentTimeMillis(), "mykey", "myValue");
+            producer.send(myMessage2);*/
+            ProducerRecord myMessage2 = new ProducerRecord(KAFKA_TOPIC, "myMessage2");
             System.out.println("Sent " + myMessage2);
+            ConsumerRecords<Long, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));
+            consumerRecords.forEach(record -> {
+                System.out.println(record.toString());
+            });
+            consumer.position(new TopicPartition(KAFKA_TOPIC, 0), Duration.ofSeconds(1));
         } finally {
             producer.close();
+            consumer.close();
         }
     }
 }
